68e04224ffca6ff414a8557ae4ea7f5cfd1c82f0
David Lopez <david.lopez@veevart.com>
2025-10-27 01:16:48 -0500

    using the repository instead of hardcoded data

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -4,48 +4,33 @@ import type CreateGameDto from './games-dto/create-game.dto';
 import { v4 as uuid } from 'uuid';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { UpdateGameDto } from './games-dto';
 
 @Injectable()
 export class GamesService {
-  games: Game[] = [
-    { id: uuid(), name: 'Hollow Knight: Silksong', status: 'playing' },
-    { id: uuid(), name: 'Final Fantasy 7 Rebirth', status: 'completed' },
-    { id: uuid(), name: 'Yakuza 0', status: 'paused' },
-    {
-      id: uuid(),
-      name: 'The Legend of Zelda: Echoes of Wisdom',
-      status: 'paused',
-    },
-    {
-      id: uuid(),
-      name: 'Metal Gear Solid Delta: Snake Eater',
-      status: 'playing',
-    },
-  ];
-
   constructor(
     @InjectRepository(Game)
-    private readonly gameRepository: Repository<Game>
+    private readonly gameRepository: Repository<Game>,
   ) {}
 
   async getGames(): Promise<Game[]> {
-    return this.games;
+    return await this.gameRepository.find();
   }
 
   async createGame(gameInput: CreateGameDto): Promise<Game[]> {
-    this.games.push({ id: uuid(), ...gameInput });
+    const game = this.gameRepository.create({ id: uuid(), ...gameInput });
+    await this.gameRepository.save(game);
     return this.getGames();
   }
 
   async deleteGame(id: string): Promise<Game[]> {
-    this.games = this.games.filter((game) => game.id !== id);
+    await this.gameRepository.delete(id);
     return this.getGames();
   }
 
-  async updateGame(gameInput: CreateGameDto): Promise<Game[]> {
-    this.games = this.games.map((game) =>
-      game.id === gameInput.id ? { ...game, ...gameInput } : game,
-    );
+  async updateGame(gameInput: UpdateGameDto): Promise<Game[]> {
+    const { id, ...updateData } = gameInput;
+    await this.gameRepository.update(gameInput.id, updateData);
     return this.getGames();
   }
 }
