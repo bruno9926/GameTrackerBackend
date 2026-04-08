af98d97990c99bb7b535fee5aee7fd615a618e9e
Bruno <david.lopez9926@gmail.com>
2026-04-08 08:00:26 -0500

    implementing Public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,25 @@
 import { Body, Post, Controller, Get, UseGuards } from "@nestjs/common";
 import { AuthService } from "./services/auth.service";
+//decorators
+import { CurrentUser } from "./decorators/current-user.decorator";
+import { IsPublic } from "./decorators/is-public.decorator";
+//guards
+import { AuthGuard } from "./guards/auth.guard";
 //dtos
 import { RegisterDto, LogInDto } from "./dtos";
-import { CurrentUser } from "./decorators/current-user.decorator";
-import { AuthGuard } from "./guards/auth.guard";
 
 @Controller('auth')
-//@UseGuards(AuthGuard)
+@UseGuards(AuthGuard)
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    @IsPublic()
     @Post('register')
     register(@Body() registerInput: RegisterDto) {
         return this.authService.registerUser(registerInput);
     }
 
+    @IsPublic()
     @Post('login')
     login(@Body() loginInput: LogInDto) {
         return this.authService.login(loginInput);
--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -3,6 +3,9 @@ import { JwtService } from "@nestjs/jwt";
 import { Request } from "express";
 import { User } from "src/users/entities";
 
+import { Reflector } from "@nestjs/core";
+import { IS_PUBLIC_KEY } from "../decorators/is-public.decorator";
+
 type UserPayload = Pick<User, "id">; // we only want to add the id
 // extending the Request interfaces so we can add the user property to it
 declare global {
@@ -15,17 +18,38 @@ declare global {
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private readonly jwtService: JwtService){}
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly reflector: Reflector
+    ) { }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        const req: Request = context.switchToHttp().getRequest();
+
+        if (this.isPublic(context)) {
+            return true;
+        }
+        return this.isAuthenticated(context);
+    }
+
+    private isPublic(context: ExecutionContext): boolean {
+        return !!this.reflector.getAllAndOverride<boolean>(
+            IS_PUBLIC_KEY,
+            [
+                context.getHandler(), //method
+                context.getClass() //class
+            ]
+        )
+    }
+
+    private async isAuthenticated(context: ExecutionContext) {
+        const req = context.switchToHttp().getRequest<Request>();
 
         const authHeader = req.headers.authorization;
         if (!authHeader) {
             throw new UnauthorizedException("Authorization header missing");
         }
 
-        const [type, token] = authHeader.split(" ");
+        const [type, token] = authHeader?.split(" ") ?? [];
         if (type !== "Bearer" || !token) {
             throw new UnauthorizedException("Invalid authorization header");
         }
