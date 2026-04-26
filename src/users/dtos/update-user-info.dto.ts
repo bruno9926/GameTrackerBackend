91a0e41856097ad46e1de77cf719e7d90276db02
Bruno <david.lopez9926@gmail.com>
2026-04-26 18:20:46 -0500

    adding more fields to UpdateUserDto

--- src/users/dtos/update-user-info.dto.ts
+++ src/users/dtos/update-user-info.dto.ts
@@ -1,7 +1,18 @@
-import { IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString, IsNotEmptyObject, IsEmail } from "class-validator";
 
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
   name?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  username?: string;
+
+  @IsOptional()
+  @IsEmail()
+  @IsNotEmpty()
+  email?: string;
 }
